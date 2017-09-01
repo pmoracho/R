472d00b0e939790717e00e11f9def3a8076114ba
Patricio <pmoracho@gmail.com>
2017-09-01 01:29:37 -0300

    version 1 of Image.Plot.R

--- Image.Plot.R
+++ Image.Plot.R
@@ -1,129 +1,100 @@
+add_image_to_color <- function(imgmagick_path, area_img_file, out_file, plot1, plot2){
+    
+    file1 <- paste0(tempfile(),"_file1.png")
+    file2 <- paste0(tempfile(),"_file2.png")
+    file3 <- paste0(tempfile(),"_file3.png")
+    file4 <- paste0(tempfile(),"_file4.png")
+    file5 <- paste0(tempfile(),"_file5.png")
+    
+    convert <- ifelse(!is.na(imgmagick_path),file.path(imgmagick_path, "convert"), "convert")
+    
+    ggsave(file1, plot = plot1, scale = 1, width = 10, height = NA, units = c("in", "cm", "mm"), dpi = 300)
+    ggsave(file2, plot = plot2, scale = 1, width = 10, height = NA, units = c("in", "cm", "mm"), dpi = 300)
+    
+    # Negar imagen
+    cmd <- paste(convert , file2, "-negate", file2)
+    system(cmd)
+
+    # Flat: me quedo solo con el area
+    cmd <- paste(convert, file1, file2, " -evaluate-sequence add -colorspace gray -auto-level -threshold 90% -negate ", file3)
+    system(cmd)
+    
+    # Largo y ancho del plot
+    cmd <- paste0(convert, " ", file1, ' -format "%wx%h" info: ')
+    pplot_dim <- unlist(lapply(strsplit(system(cmd, intern = TRUE), "x"), as.integer))
+    
+    # Redimensiono mÃ¡scara
+    cmd <- paste(convert, area_img_file, "-resize", paste0(pplot_dim[1],"x",pplot_dim[2], "!"), file4 )
+    system(cmd)
+    
+    # Combinar capas para armar el area con la imagen y el fondo transparente
+    cmd <- paste(convert, "-gravity Center -geometry +0+0", file4,  "\\(", file3, "-gravity Center -geometry +0+0 \\)", "-set colorspace RGB -alpha off -geometry +0+0 -compose copy_opacity -composite -set colorspace sRGB", file5)
+    system(cmd)
+    
+    # CombinaciÃ³n final, el grafico original con la imagen superpuesta en el area
+    cmd <- paste(convert, file1, file5, "-flatten -blur 1x65000", out_file)
+    system(cmd)        
+}
+
 library(datasets)
 library(ggplot2)
-library(magick)
 
 rm(list = ls())
 
-area_img_file <- file.path(getwd(),"ozono.jpg")
-out_file <- file.path(getwd(), "final.png")
-imgmagick_path <- "D:/pm/bin/ImageMagick-7.0.6-10"
-
-convert <- file.path(imgmagick_path, "convert.exe")
-composite <- file.path(imgmagick_path, "composite.exe") 
-
 data(airquality)
 
-file1 <- paste0(tempfile(),"_file1.png")
-file2 <- paste0(tempfile(),"_file2.png")
-file3 <- paste0(tempfile(),"_file3.png")
-file4 <- paste0(tempfile(),"_file4.png")
-file5 <- paste0(tempfile(),"_file5.png")
+area_img_file <- file.path(getwd(),"dolar.jpg")
+out_file <- file.path(getwd(), "final.png")
+imgmagick_path <- NA
 
 # Genero la grafica
-plot <- ggplot(airquality, aes(x = Ozone, colour=)) +
-        geom_density(fill = "#4271AE", colour = "#CC6666", alpha = 1) +
-        scale_x_continuous(name = "Promedio de Ozono en\npartes por billones",
-                           breaks = seq(0, 200, 25),
-                           limits=c(0, 200)) +
-        scale_y_continuous(name = "Density") +
-        ggtitle("Gráfico de densidad del promedio de ozono")
-
-ggsave(file1, scale = 1, width = 10, height = NA, units = c("in", "cm", "mm"), dpi = 300)
-
-plot <- plot + geom_density(fill = "#CC6666", colour = "#CC6666", alpha = 1)
-ggsave(file2, scale = 1, width = 10, height = NA, units = c("in", "cm", "mm"), dpi = 300)
-
-
-# Negar imagen
-cmd <- paste(convert , file2, "-negate", file2)
-system(cmd)
-
-# Flat: me quedo solo con el area
-cmd <- paste(composite, file1, file2, " -evaluate-sequence add -colorspace gray -auto-level -threshold 90% -negate ", file3)
-system(cmd)
-
-# Largo y ancho del plot
-cmd <- paste0(convert, " ", file1, ' -format "%wx%h" info: ')
-pplot_dim <- unlist(lapply(strsplit(system(cmd, show.output.on.console=TRUE, intern = TRUE), "x"), as.integer))
-
-# Largo y ancho de la imagen a suporponer
-cmd <- paste0(convert, " ", area_img_file, ' -format "%wx%h" info: ')
-area_dim <- unlist(lapply(strsplit(system(cmd, show.output.on.console=TRUE, intern = TRUE), "x"), as.integer))
-
-# Redimensiono máscara
-cmd <- paste(convert, area_img_file, "-resize", paste0(pplot_dim[1],"x",pplot_dim[2], "!"), file4 )
-system(cmd)
-
-# Combinar capas para armar el area con la imagen y el fondo transparente
-cmd <- paste(convert, "-gravity Center -geometry +0+0", file4,  "( ", file5, "-gravity Center -geometry +0+0 ) ", "-set colorspace RGB -alpha off -geometry +0+0 -compose copy_opacity -composite -set colorspace sRGB", file5)
-system(cmd)
-
-# Combinación final, el grafico original con la imagen superpuesta en el area
-cmd <- paste(composite, file1, file5, "-flatten -blur 1x65000", out_file)
-system(cmd)
-
-# image.png ( mask.png -gravity Center ) -set colorspace RGB -alpha off -compose copy_opacity -composite -set colorspace sRGB -gravity center salida
-# convert prueba3.png -colorspace gray -auto-level -threshold 90% -negate prueba4.png
-# composite -compose Dst_In \( prueba4.png -alpha copy \) dolar.jpg -alpha Set  PNG32:salida.png
-# composite  salida.png prueba1.png out.png
-
-#' Obtener el tamaño en pixels de una imagen
-#'
-#' @param imgfile Ptah a la imagen
-#' @keywords image imagemagick
-#' @export
-#' @examples
-#' size <- get_imgsize("prueba.jpg")
-
-get_imgsize<-function(imgfile) {
-
-    cmd <- paste0(convert, " ", imgfile, ' -format "%wx%h" info: ')
-    ret <- system(cmd, show.output.on.console=TRUE, intern = TRUE)
-    return(unlist(lapply(strsplit(ret, "x"), as.integer)))
-}
-
-r <- get_imgsize(file3)
-r[2]
-class(r)
-
-
-resize<-function(from, to) {
-    
-}
-
-
-dolar <- image_crop(image_scale(dolar, "x2100"), "2100x2100")
-out <- image_negate(new)
-out <- image_flatten(c(dolar, mask))
-out <- image_crop("2100x2100+0")
-
-g <- rasterGrob(backimg, interpolate=TRUE)
-
-library(jpeg)
-library(grid)
-g <- rasterGrob(backimg, interpolate=TRUE)
-
-backimg <- image_read("/home/pmoracho/prueba6.png")
-
-newplot <- plot + annotation_custom(rasterGrob(out, width=unit(1,"npc"), height=unit(1,"npc")), 
-                         -Inf, Inf, -Inf, Inf)
-plot
-
-myColours = c(1, "steelblue", "#FFBB00", rgb(0.4, 0.2, 0.3))
-myColoursAlpha <- add.alpha(myColours, alpha=0.4)
-
-add.alpha <- function(col, alpha=1){
-    if(missing(col))
-        stop("Please provide a vector of colours.")
-    apply(sapply(col, col2rgb)/255, 2, 
-          function(x) 
-              rgb(x[1], x[2], x[3], alpha=alpha))  
-}
-
-plot <- ggplot(airquality, aes(x = Ozone)) +
-    geom_density(fill = "#4271AE", colour = "#4271AE", alpha = 1) +
-    scale_x_continuous(name = "Mean ozone in\nparts per billion",
+plot1 <- ggplot(airquality, aes(x = Ozone)) +
+    geom_density(fill = "#4271AE", colour = "#CC6666", alpha = 1) +
+    scale_x_continuous(name = "Promedio de Ozono en\npartes por billones",
                        breaks = seq(0, 200, 25),
                        limits=c(0, 200)) +
     scale_y_continuous(name = "Density") +
-    ggtitle("Density plot of mean ozone")
+    ggtitle("GrÃ¡fico de densidad del promedio de ozono")
+
+plot2 <- plot1 + geom_density(fill = "#CC6666", colour = "#CC6666", alpha = 1)
+
+add_image_to_color(imgmagick_path, area_img_file, out_file, plot1, plot2)
+
+
+
+set.seed(1234)
+df <- data.frame(
+    sex=factor(rep(c("F", "M"), each=200)),
+    weight=round(c(rnorm(200, mean=55, sd=5),
+                   rnorm(200, mean=65, sd=5)))
+)
+
+plot1 <- ggplot(df, aes(x=weight)) +
+    geom_area(stat = "bin", fill = "lightblue", color="darkblue")+
+    geom_vline(aes(xintercept=mean(weight)),
+               color="blue", linetype="dashed", size=1)
+
+plot2 <- ggplot(df, aes(x=weight)) +
+    geom_area(stat = "bin", fill = "red", color="darkblue")+
+    geom_vline(aes(xintercept=mean(weight)),
+               color="blue", linetype="dashed", size=1)
+
+add_image_to_color(imgmagick_path, area_img_file, out_file, plot1, plot2)
+
+
+
+months <-rep(c("jan", "feb", "mar", "apr", "may", "jun", 
+               "jul", "aug", "sep", "oct", "nov", "dec"), 2)
+chickens <-c(1, 2, 3, 3, 3, 4, 5, 4, 3, 4, 2, 2)
+eggs <-c(0, 8, 10, 13, 16, 20, 25, 20, 18, 16, 10, 8)
+values <-c(eggs)
+type <-c(rep("eggs", 12))
+mydata <-data.frame(months, values)
+
+plot1 <-ggplot(mydata, aes(months, values)) +
+    geom_bar(stat = "identity", fill="red", color="darkblue", aes(fill = type)) 
+
+plot2 <-ggplot(mydata, aes(months, values)) +
+    geom_bar(stat = "identity", fill="darkblue", color="darkblue", aes(fill = type)) 
+
+add_image_to_color(imgmagick_path, area_img_file, out_file, plot1, plot2)
