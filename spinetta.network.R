c082874d8fe09354615fa5605a69cecd85f6bcf1
Patricio Moracho <pmoracho@gmail.com>
2020-01-09 00:45:42 -0300

    Spinneta network y más ejercicios de economía

---
+++ spinetta.network.R
@@ -0,0 +1,19 @@
+library(igraph)
+library(networkD3)
+
+
+# create a dataset:
+data <- read.csv("spinetta.data.csv", sep = ";", header = TRUE)
+
+simpleNetwork(data, height="100px", width="100px",        
+                   Source = 1,                 # column number of source
+                   Target = 2,                 # column number of target
+                   linkDistance = 10,          # distance between node. Increase this value to have more space between nodes
+                   charge = -900,                # numeric value indicating either the strength of the node repulsion (negative value) or attraction (positive value)
+                   fontSize = 14,               # size of the node names
+                   fontFamily = "serif",       # font og node names
+                   linkColour = "#666",        # colour of edges, MUST be a common colour for the whole graph
+                   nodeColour = "#69b3a2",     # colour of nodes, MUST be a common colour for the whole graph
+                   opacity = 0.9,              # opacity of nodes. 0=transparent. 1=no transparency
+                   zoom = T                    # Can you zoom on the figure?
+)
