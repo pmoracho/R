67b861d14822a0455d125c0068a60c0c7439d7ec
Patricio Moracho <pmoracho@gmail.com>
2020-05-14 00:09:46 -0300

    Graficos del día 3

--- 30_diasdegraficosenr_dia03.R
+++ 30_diasdegraficosenr_dia03.R
@@ -1,6 +1,6 @@
 # 30díasdegráficos con R - día 3 - Puntos
 # Tema personalizado: devtools::install_github("pmoracho/ggelegant")
-# Gráficos: Ggplo2 + Algo de dplyr
+# Gráficos: Ggplo2 + ggrepel + Algo de dplyr
 # Font: Ralleway
 # Data: https://opendata.ecdc.europa.eu/covid19/casedistribution/csv
 # Para #30díasdegráficos y #rstatsES. Día 3: ¿Hay alguna relación entre el índice de desarrollo humano de cada país y 
@@ -8,6 +8,7 @@
 # Github: https://github.com/pmoracho/R/blob/master/30_diasdegraficosenr_dia03.R
 
 library("tidyverse")
+library("ggrepel")
 
 if ("ggelegant" %in% rownames(installed.packages())) {
   library("ggelegant")
@@ -26,9 +27,10 @@ hdi %>%
   slice(n()) %>% 
   select(country_code, country, year, value) -> last_hdi
 
-last_date <- max(as.Date(covid.data$dateRep,"%d/%m/%Y"))
-paises_de_interes <- c('Argentina','Brazil', 'Chile', 'Bolivia', 'Paraguay', 'Uruguay')
 
+last_date <- max(as.Date(covid.data$dateRep,"%d/%m/%Y"))
+paises_de_interes <- c( 'Argentina',
+                       "Niger", "Norway", "EEUU", "Mauritania")
 covid.data %>% 
   group_by(countriesAndTerritories, countryterritoryCode) %>% 
   summarize(casos = sum(cases), fallecidos = sum(deaths)) %>% 
@@ -36,10 +38,18 @@ covid.data %>%
   inner_join(last_hdi,
             by = c("countryterritoryCode" = "country_code")
             ) %>% 
-  select(pais = countriesAndTerritories, casos, fallecidos, HDI = value) %>% 
+  mutate(pais = ifelse(countriesAndTerritories == 'United_States_of_America', 'EEUU', countriesAndTerritories)) %>% 
+  select(pais, casos, fallecidos, HDI = value) %>% 
+  mutate(pais_etiquetado = ifelse(pais %in% paises_de_interes, paste0(pais, " (casos: ", format(casos, digits=0, big.mark = ',', trim=TRUE), " hdi: ", HDI, ")"), NA)) %>% 
   ggplot(aes(x=HDI, y=casos)) +
     geom_point(color = "#67a9cf", alpha=.5, size=3) +
     geom_smooth(method = 'lm',formula='y ~ x', se=FALSE, color="#ef8a62") +
+    geom_label_repel(mapping = aes(label = pais_etiquetado),
+                     color="#67a9cf",family = "Ralleway", vjust = -1.2, hjust = 1.1) +
+                     # nudge_x = 1, nudge_y = 5, color="#67a9cf",
+                     # vjust = -2, family = "Ralleway",  
+                     # direction  = "y",
+                     # hjust = 2) +
     scale_y_log10(
       breaks = scales::trans_breaks("log10", function(x) 10^x),
       labels = scales::trans_format("log10", scales::math_format(10^.x))
