7d77250811e110c5437c2f7c374440ddd6e19d53
Patricio Moracho <pmoracho@gmail.com>
2020-05-31 14:33:09 -0300

    Dia 20

--- 30_diasdegraficosenr_dia20.R
+++ 30_diasdegraficosenr_dia20.R
@@ -1,13 +1,15 @@
-# 30díasdegráficos con R - día 16 - Sankey/alluvial
+# 30díasdegráficos con R - día 20 - Redes
 # Tema personalizado: devtools::install_github("pmoracho/ggelegant")
-# Gráficos: Ggplo2 + ggalluvial + Algo de dplyr
+# Gráficos: Ggplo2 + ggraph + Algo de dplyr
 # Font: Ralleway
-# Para #30díasdegráficos y #rstatsES. Día 17: Una grafico Sankey o alluvial de como es la evolución de los casos de COVID-19 en Argentina, 
-# por grupo etario y sex0.
-# Github: https://github.com/pmoracho/R/blob/master/30_diasdegraficosenr_dia17.R
+# Para #30díasdegráficos y #rstatsES. Día 20: Un gráfico de redes para observar como se distribuyen las internaciones
+# según el rango etario y el sexo.
+# Github: https://github.com/pmoracho/R/blob/master/30_diasdegraficosenr_dia20.R
 
 library("tidyverse")
-library("ggalluvial")
+library("ggraph")
+library("igraph")
+
 if ("ggelegant" %in% rownames(installed.packages())) {
   library("ggelegant")
 } else {
@@ -35,30 +37,38 @@ covid.data %>%
                                  edad_actual_anios > 35 & edad_actual_anios <= 65 ~ '36 a 65',                          
                                  edad_actual_anios > 65 ~ '>= 66')) %>% 
   select(sexo, edad=edad_actual_anios, clasif_edad, internado, cui, arm, fallecido) %>%
-  mutate(clasif_edad = factor(clasif_edad, c('0 a 6', '7 a 14', '15 a 35', '36 a 65', '>= 66')),
-         internado = factor(internado, c("Ambulatorio", "Internado")),
-         fallecido = factor(fallecido, c("Recuperado", "Fallecido"))
-         ) %>% 
   group_by(clasif_edad, sexo, internado, fallecido) %>% 
-    summarise(n = n()) -> plot_data
+  summarise(n = n()) -> plot_data
+
 
 plot_data %>% 
-  ggplot(mapping=aes(y = n,
-                     axis1 = fallecido, axis2 = internado, axis3 = sexo, axis4 = clasif_edad)) +
-  geom_alluvium(aes(fill = clasif_edad),
-                color = "Gray30",
-                width = 0, knot.pos = 0.1, reverse = FALSE) +
-  guides(fill = FALSE) +
-  geom_stratum(width = 1/8, reverse = FALSE, color="gray60", linetype = 3) +
-  geom_text(stat = "stratum", infer.label = TRUE, reverse = FALSE) +
-  scale_x_continuous(breaks = 1:4, labels = c("Fallecido", "Internado", "Sexo", "Edad")) +
-  coord_flip() +
+  group_by(sexo, clasif_edad) %>% 
+  summarise(n=sum(n)) %>% 
+  select(from=sexo, to=clasif_edad, n) -> sexo_edad
+
+plot_data %>% 
+  group_by(internado, clasif_edad) %>% 
+  summarise(n=sum(n)) %>% 
+  select(to=internado, from=clasif_edad, n) -> internado_edad
+
+internado_edad %>%
+  rbind(sexo_edad) %>% 
+  graph_from_data_frame() %>% 
+  ggraph(layout = 'stress') +
+  geom_edge_link(aes(edge_width=n), show.legend = TRUE, alpha=0.5, color = "#67a9cf") +
+  geom_node_point(size=8, color ="#ef8a62") +
+  geom_node_text(aes(label = name), repel = TRUE, family="Ralleway") +
+  theme_elegante_std(base_family = "Ralleway") +
   labs(title = paste("COVID-19 en Argentina"), 
-       subtitle = paste0("Evolución de los caso por edad y sexo (al: ", last_date, ")") , 
-         caption = "Fuente: datos.gob.ar", 
-       y = "Casos Confirmados", 
+       subtitle = paste0("¿Cómo se distribuyen las internaciones entre el sexo y la edad?\nDatos al: ", last_date) , 
+       caption = "Fuente: https://datos.gob.ar", 
+       y = "", 
        x = ""
   ) +
-  scale_fill_manual(values = c("firebrick3", "darkorange", "deepskyblue3", "darkorchid1", "seagreen")) +
-  scale_color_manual(values = rep("Black",5)) +
-  theme_elegante_std(base_family = "Ralleway")
+  guides(fill = guide_legend(nrow = 1)) +
+  theme(axis.text.x=element_blank(),
+        axis.ticks.x=element_blank(),
+        axis.title.y=element_blank(),
+        axis.text.y=element_blank(),
+        axis.ticks.y=element_blank()) 
+
